eda7c3385b429b544b5ff6dd8dc227de4ee35aa0
anuj-bacancy <anuj.sharma@bacancy.com>
2026-03-14 20:30:09 +0530

    fix build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  distDir: ".next-build",
+  distDir: ".next-local",
 };
 
 export default nextConfig;
